0c0fef67c9159c4d9bb4262a982d3bc0e7b0dba7
keqc <keqc@dataexa.com>
2020-08-19 20:15:44 +0800

    11

--- app-mapper/src/main/java/com/devilkxh/app/mapper/AppGameScoreMapper.java
+++ app-mapper/src/main/java/com/devilkxh/app/mapper/AppGameScoreMapper.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 public interface AppGameScoreMapper extends BaseMapper<AppGameScore> {
 
-    @Select(" select u.USER_NAME userName, case when t.score is null then 0 else t.score end score from APP_USER u " +
+    @Select(" select u.USER_NAME userName, u.USER_LOGO userLogo, case when t.score is null then 0 else t.score end score from APP_USER u " +
             " left join (" +
             " select OPEN_ID, sum(score) score FROM APP_GAME_SCORE " +
             " group by OPEN_ID) t on u.open_id = t.OPEN_ID" +
