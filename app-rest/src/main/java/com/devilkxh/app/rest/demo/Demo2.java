210786a2f03038489c33dbc5c71bb92d73fc51c4
kexiaohong <752527107@qq.com>
2020-09-20 22:39:45 +0800

    m

--- app-rest/src/main/java/com/devilkxh/app/rest/demo/Demo2.java
+++ app-rest/src/main/java/com/devilkxh/app/rest/demo/Demo2.java
@@ -6,43 +6,94 @@ package com.devilkxh.app.rest.demo;
  */
 public class Demo2 {
 
-    public boolean repeatedSubstringPattern(String s) {
+    public boolean isNumber(String s) {
         int len = s.length();
-        int repeat = 1;
-        boolean flag = false;
-        while(repeat <= len / 2) {
-            if(len % repeat != 0) {
-                repeat ++;
+        int num = 0;
+        int point = 0;
+        if(len == 0) {
+            return false;
+        }
+        s = s.trim();
+        int index = 0;
+        if(s.charAt(index) == '+' || s.charAt(index) == '-') {
+            index ++;
+        }
+        for(;index < len; index ++) {
+            char c = s.charAt(index);
+            if(c == '.' && point == 0) {
+                point ++;
                 continue;
             }
-            flag = true;
-            for(int i = 0; i < repeat; i ++) {
-                char c = s.charAt(i);
-                int j = repeat + i;
-                while (j < len) {
-                   if(c != s.charAt(j)) {
-                       flag = false;
-                       break;
-                   } else {
-                       j += repeat;
-                   }
-                }
-                if(!flag) {
-                    break;
-                }
+            if(!isNum(c)) {
+                return false;
             }
-            if(flag) {
-                break;
+
+            if(c == 'e' || c == 'E') {
+                if(num == 0) {
+                    return false;
+                }
+                return isAllNum(index + 1, s);
+            } else {
+                num ++;
             }
-            repeat ++;
         }
-        return flag;
+        if(num != 0) {
+            return true;
+        }
+        return false;
     }
 
+    public boolean isAllNum(int index, String s) {
+        int len = s.length();
+        int num = 0;
+        if(len == 0 || index == len) {
+            return false;
+        }
+        if(s.charAt(index) == '+' || s.charAt(index) == '-') {
+            index ++;
+        }
+        for(;index < len; index ++) {
+            char c = s.charAt(index);
+            if(!isRealNum(c)) {
+                return false;
+            }
+            num ++;
+        }
+        if(num == 0) {
+            return false;
+        }
+        return true;
+
+    }
+
+    public boolean isNum(char c) {
+        return isRealNum(c) || isENum(c);
+    }
+
+    public boolean isRealNum(char c) {
+        if(c >= '0' && c <= '9') {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isENum(char c) {
+        if(c == 'e' || c == 'E') {
+            return true;
+        }
+        return false;
+    }
+
+
     public static void main(String []agrs) {
-//        System.out.println(new Demo2().repeatedSubstringPattern("abac"));
-        System.out.println(new Demo2().repeatedSubstringPattern("ababab"));
-//        System.out.println(new Demo2().repeatedSubstringPattern("aba"));
-//        System.out.println(new Demo2().repeatedSubstringPattern("abcabcabcabc"));
+        System.out.println(new Demo2().isNumber("1"));
+        System.out.println(new Demo2().isNumber("5e2"));
+        System.out.println(new Demo2().isNumber("-123"));
+        System.out.println(new Demo2().isNumber("-1E-16"));
+        System.out.println(new Demo2().isNumber("12e"));
+        System.out.println(new Demo2().isNumber("1a3.14"));
+        System.out.println(new Demo2().isNumber("1.2.3"));
+        System.out.println(new Demo2().isNumber("+-5"));
+        System.out.println(new Demo2().isNumber("12e+5.4"));
     }
 }
