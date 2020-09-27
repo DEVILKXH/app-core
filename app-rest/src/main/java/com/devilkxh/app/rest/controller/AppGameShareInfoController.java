2305c575f48036f6fbab9e554ec7cca5f9da23bb
keqc <keqc@dataexa.com>
2020-09-27 22:53:43 +0800

    m

--- app-rest/src/main/java/com/devilkxh/app/rest/controller/AppGameShareInfoController.java
+++ app-rest/src/main/java/com/devilkxh/app/rest/controller/AppGameShareInfoController.java
@@ -29,16 +29,39 @@ public class AppGameShareInfoController {
     @PostMapping(value = "/save")
     public ResultBean save(@RequestBody AppGameShareInfo shareInfo) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        QueryWrapper wrapper2 = new QueryWrapper();
+        wrapper2.eq(AppGameShareInfoColumn.SHARE_OPEN_ID.toString(), shareInfo.getShareOpenId()).
+                and().eq(AppGameShareInfoColumn.SHARE_DATE.toString(), sdf.format(new Date()));
+
+        int count = shareInfoService.selectCount(wrapper2);
+        if (count >= 5) {
+            return ResponseHelper.success("您今天助力次数达到上限，不能在助力了哦");
+        }
+
+        QueryWrapper wrapper3 = new QueryWrapper();
+        wrapper3.eq(AppGameShareInfoColumn.OPEN_ID.toString(), shareInfo.getOpenId()).
+                and().eq(AppGameShareInfoColumn.SHARE_DATE.toString(), sdf.format(new Date()));
+
+        count = shareInfoService.selectCount(wrapper3);
+        if (count >= 3) {
+            return ResponseHelper.success("对方今天助力次数达到上限，不能在助力了哦");
+        }
+
         QueryWrapper wrapper = new QueryWrapper();
         wrapper.eq(AppGameShareInfoColumn.OPEN_ID.toString(), shareInfo.getOpenId()).
                 and().eq(AppGameShareInfoColumn.SHARE_OPEN_ID.toString(), shareInfo.getShareOpenId()).
                 and().eq(AppGameShareInfoColumn.SHARE_TYPE.toString(), shareInfo.getShareType().toUpperCase()).
                 and().eq(AppGameShareInfoColumn.SHARE_DATE.toString(), sdf.format(new Date()));
 
-        int count = shareInfoService.selectCount(wrapper);
+        count = shareInfoService.selectCount(wrapper);
         if (count != 0) {
             return ResponseHelper.success("您今天已经助力过了，不能在助力了哦");
         }
+
+
+
+
         shareInfo.setShareDate(sdf.format(new Date()));
         shareInfo.setUuid(UUID.randomUUID().toString());
         shareInfo.setShareType(shareInfo.getShareType().toUpperCase());
